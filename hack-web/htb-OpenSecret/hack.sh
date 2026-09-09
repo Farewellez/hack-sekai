3f02e6442834c01794a6c1bf379a6789a0af398d
Farewellez <ahmadzafarell55@gmail.com>
2026-09-10 01:13:16 +0700

    Vuln: just inspect the element

---
+++ hack-web/htb-OpenSecret/hack.sh
@@ -0,0 +1,8 @@
+curl 154.57.164.66:31647 | grep HTB
+
+# hack-sekai/hack-web/htb-OpenSecret on  main [?] via 🐍 v3.14.7 (hack-sekai_env) 
+# ❯ curl 154.57.164.66:31647 | grep HTB
+#   % Total    % Received % Xferd  Average Speed  Time    Time    Time   Current
+#                                  Dload  Upload  Total   Spent   Left   Speed
+# 100   8375 100   8375   0      0            const SECRET_KEY = "HTB{0p3n_s3cr3ts_ar3_n0t_s3cr3ts}";
+# 100   8375 100   8375   0      0  16173      0                              0
